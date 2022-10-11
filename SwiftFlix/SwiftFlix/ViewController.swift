d9af929aa681d8378615809c79357338ce019db0
Kell Lanes <kethreen.lanes_ciandt@prestador.globo>
2022-10-11 13:40:46 -0300

    configurando view code

---
+++ SwiftFlix/SwiftFlix/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SwiftFlix
+//
+//  Created by Larissa de Souza Lanes Goncalves on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+}
